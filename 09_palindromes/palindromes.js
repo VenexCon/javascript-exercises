0f68404f9343778a2ddf5f7f78444d27f28090a9
Connor Davies <96823004+VenexCon@users.noreply.github.com>
2022-05-14 12:21:39 +0100

    <update> Updated regex for function palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,24 +2,24 @@
 
 
 // make letters consistant 
-// remove numbers 
+// remove numbers - Look up REgex
 // split characters into an array 
 // reverse the array 
 // Use an if, or for loop to sort the array and see if characters match. 
 // -- * possible use a for loop and see if that works 
 // return a boolean value to see if it is true or false. 
 
-const palindromes = function isPalindromes (string) {
-    let Array1 = Array.from(string.toLowerCase()); 
-    let Array2 =  Array.from(string.toLowerCase());
-    Array2.reverse();
-  console.log(Array2)
-  console.log(Array1)
-    if(Array1.toString() == Array2.toString()) {
-      return true; 
-    } else {
-      return false; 
-    }
+const palindromes = function isPalindrome (string) {
+  let newString = string.toLowerCase().replace(/[^a-z]/g, "");
+  let Array1 = Array.from(newString.toLowerCase()); 
+  let Array2 = Array.from(newString.toLowerCase());
+      Array2.reverse();
+
+  if(Array1.toString() == Array2.toString()) {
+    return true; 
+  } else{
+    return false; 
+  }
 };
 
 
