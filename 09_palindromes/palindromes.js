e3743e71a892a8fe0a557c271596477cf08dbb65
Connor Davies <96823004+VenexCon@users.noreply.github.com>
2022-05-14 11:58:00 +0100

    <update> Palindrome Fucntion Created

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
 
+
+
+// make letters consistant 
+// remove numbers 
+// split characters into an array 
+// reverse the array 
+// Use an if, or for loop to sort the array and see if characters match. 
+// -- * possible use a for loop and see if that works 
+// return a boolean value to see if it is true or false. 
+
+const palindromes = function isPalindromes (string) {
+    let Array1 = Array.from(string.toLowerCase()); 
+    let Array2 =  Array.from(string.toLowerCase());
+    Array2.reverse();
+  console.log(Array2)
+  console.log(Array1)
+    if(Array1.toString() == Array2.toString()) {
+      return true; 
+    } else {
+      return false; 
+    }
 };
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
